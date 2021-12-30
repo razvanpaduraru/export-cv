0dcfd31e92c9bbd44e6c225cec12a4d0697a9e4a
razvanpaduraru <razvan.paduraru@stud.acs.upb.ro>
2021-12-30 13:28:15 +0200

    First Disertation Commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/big-application'),
+      dir: require('path').join(__dirname, './coverage/export-cv'),
       subdir: '.',
       reporters: [
         { type: 'html' },
